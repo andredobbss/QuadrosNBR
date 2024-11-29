a8a83736f6828fc7800e021054c062273869e0ef
André Dobbss <67844894+andredobbss@users.noreply.github.com>
2024-11-29 18:02:49 -0300

    Ajustes gerais

--- Dominio/Entities/MemoriaDominio.cs
+++ Dominio/Entities/MemoriaDominio.cs
@@ -52,16 +52,16 @@ namespace QuadrosNBR.Dominio.Entities
 
         public string NomeDaLayer { get; private set; }
         public decimal Area { get; private set; }
-        public string Descricao { get; private set; }
+        public string? Descricao { get; private set; } = string.Empty;
         public uint Repeticao { get; private set; }
-        public string Unidade { get; private set; }
-        public string Pavimento { get; private set; }
-        public string Uso { get; private set; }
+        public string? Unidade { get; private set; } = string.Empty;
+        public string? Pavimento { get; private set; } = string.Empty;
+        public string? Uso { get; private set; } = string.Empty;
         public decimal Coeficiente { get; private set; }
         public bool AreaCobertaAberta { get; private set; }
         public bool Proporcionalidade { get; private set; }
         public bool Acessoria { get; private set; }
-        public string Observacao { get; private set; }
+        public string? Observacao { get; private set; } = string.Empty;
         public bool Terreno { get; private set; }
         public double AreaCobertaPadrao { get; private set; }
         public double AreaDiferenteDaCobertaPadrao { get; private set; }
