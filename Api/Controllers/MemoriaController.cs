e14d119be4ddc8932a636a551cdf313318eaca03
André Dobbss <67844894+andredobbss@users.noreply.github.com>
2024-12-02 06:55:02 -0300

    alteracão no controller MemoriaController

--- Api/Controllers/MemoriaController.cs
+++ Api/Controllers/MemoriaController.cs
@@ -24,45 +24,21 @@ public class MemoriaController : ControllerBase
         _mapper = mapper;
     }
 
-    // GET: api/<MemoriaController>
-    [HttpGet]
-    public ActionResult<string> Get([FromRoute] string projid, string tenantId, string dwgFilePath)
+   
+    [HttpPost]
+    public ActionResult<string> PostAreaAutocad([FromRoute] string projid, string tenantId, string dwgFilePath)
     {
         var area =  _unitOfWork.Imemoria.AreaAutocad(Guid.Parse(projid), Guid.Parse(tenantId), dwgFilePath);
      
-        if (area == false)
+        if (area is true)
         {
-            return NotFound();
+            return Ok();
         }
         else
         {
-            return Ok();
+            return NotFound();
         }  
     }
 
-    // GET api/<MemoriaController>/5
-    [HttpGet("{id}")]
-    public string Get(int id)
-    {
-        return "value";
-    }
-
-    // POST api/<MemoriaController>
-    [HttpPost]
-    public void Post([FromBody] string value)
-    {
-        _unitOfWork.Commit();
-    }
-
-    // PUT api/<MemoriaController>/5
-    [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
-    {
-    }
-
-    // DELETE api/<MemoriaController>/5
-    [HttpDelete("{id}")]
-    public void Delete(int id)
-    {
-    }
+   
 }
